4a53b077156d18f69d0f51c1d681cbc9bb6ef0b4
Tatianna Martinez <tatianna.martinez71@gmail.com>
2024-03-08 19:26:41 +0000

    Save progress: 620.1

--- countdown.sh
+++ countdown.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Program that counts down to zero from a given argument
-if [[ $1 -gt 0 ]]
-then
-  echo true
-else 
-  echo Include a positive integer as the first argument.
-fi
+
+
+for (( i = $1; i > 0; i-- ))
+do
+  echo $i
+done
